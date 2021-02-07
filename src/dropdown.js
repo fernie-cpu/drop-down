862b24502fe9d568117606b5a484b52087ac60b8
fernie-cpu <yoloisjustanick@gmail.com>
2021-02-06 23:44:00 -0300

    add function to show and hide dropdown

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,5 +1,16 @@
 function dropDown() {
-  const drop = document.querySelector('.links');
+  const drop = document.getElementById('dropdown');
+  const down = document.querySelector('.dropList');
+
+  drop.addEventListener('click', (e) => {
+    e.stopPropagation();
+    down.classList.toggle('active');
+  });
 }
+
+window.addEventListener('click', () => {
+  const hide = document.querySelector('.dropList');
+  hide.classList.remove('active');
+});
 
 export { dropDown };
